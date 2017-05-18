6604bb2b469df27696ea7313775102bdbd8dd7b2
m9rcy <m9rcy19@gmail.com>
2017-05-18 21:33:08 +0800

    Revert "add hello back again"
    This reverts commit ac1e38b60f2789cb61da09ff5016a3a03555f766.

--- hello-world/src/com/gabs/git/Hello.java
+++
@@ -1,5 +0,0 @@
-package com.gabs.git;
-
-public class Hello {
-
-}
--- hello-world/src/com/gabs/git/world.java
+++ hello-world/src/com/gabs/git/world.java
@@ -5,7 +5,7 @@ public class world {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("world");
-		Hello hello = new Hello();
+
 	}
 
 }
